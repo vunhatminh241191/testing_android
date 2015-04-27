89684c5f8b3afc03bde3e0609758ce21c4d48634
Minh Vu <vnm241191@gmail.com>
2015-04-26 20:11:55 -0700

    Adding Mandrill app to sending and receiving message

--- app/config.py
+++ app/config.py
@@ -1,1 +1,6 @@
+from os import environ
 SQLALCHEMY_DATABASE_URI = 'postgresql://testing_android:testing123@localhost/android'
+MANDRILL_API_KEY = environ.get('MANDRILL_API_KEY')
+
+EMAIL_BACKEND = "djrill.mail.backends.djrill.DjrillBackend"
+EMAIL_HOST = 'smtp.mandrillapp.com'
--- app/views.py
+++ app/views.py
@@ -1,6 +1,9 @@
 from app import app, db
 from flask import request, jsonify, make_response, abort
 from models import Account
+import datetime
+import mandrill
+from config import MANDRILL_API_KEY
 
 @app.route('/')
 def index():
@@ -20,7 +23,7 @@ def accounts():
 				json_results.append(d)
 			return jsonify(items=json_results)
 
-@app.route('/accounts/<int:account_id>', methods=['GET'])
+@app.route('/account/<int:account_id>', methods=['GET'])
 def get_account(account_id):
 	account = Account.query.filter_by(id=account_id).first()
 	if account == None:
@@ -36,24 +39,24 @@ def post_accounts():
 	if request.method == 'POST':
 		username = request.json.get('username')
 		password = request.json.get('password')
-		signup_date = request.json.get('signup_date')
 		email = request.json.get('email')
+		facebook_id = request.json.get('facebook_id')
 
 		account = Account.query.filter_by(username=username).first()
 
 		if username is None or password is None:
 			abort(400) # missing arguments
 		if account is not None:
-			if verify_password(username, password) == True:
-				return "Login Success"
-			else:
-				return "Username or password is not correct"
-			
-		user = Account(username=username, signup_date=signup_date, email=email)
-		user.hash_password(password)
-		db.session.add(user)
-		db.session.commit()
-		return "Adding successful"
+			return login(username, password)
+		else:
+			return signup(username, password, email, facebook_id)
+
+@app.route('/account/<int:account_id>', methods=['DELETE'])
+def delete_account(account_id):
+	account = Account.query.filter_by(id=account_id).first()
+	db.session.delete(account)
+	db.session.commit()
+	return "Delete successful"
 
 @app.errorhandler(404)
 def not_found(error):
@@ -64,7 +67,38 @@ def bad_request(error):
 	return make_response(jsonify({'error':'Bad Request'}), 400)
 
 def verify_password(username, password):
-    user = Account.query.filter_by(username = username).first()
-    if not user or not user.verify_password(password):
-        return False
-    return True
+	user = Account.query.filter_by(username = username).first()
+	if not user or not user.verify_password(password) or user.email:
+		return False
+	return True
+
+def login(username, password):
+	if verify_password(username, password) == True:
+		return "Login Success"
+	else:
+		return "Username or password is not correct"
+
+def signup(username, password, email, facebook_id):
+	user = Account(username=username
+		,signup_date=datetime.date.today().strftime("%Y-%m-%d")
+		,email=email, facebook_id=facebook_id)
+	user.hash_password(password)
+	db.session.add(user)
+	db.session.commit()
+	send_mail('testing_android', [user.email], {})
+	return "Adding successful"
+
+def send_mail(template_name, email_to, context):
+	mandrill_client = mandrill.Mandrill(MANDRILL_API_KEY)
+	message = {
+		'to': [],
+		'global_merge_vars': []
+	}
+	for em in email_to:
+		message['to'].append({'email': em})
+ 
+	for k, v in context.iteritems():
+		message['global_merge_vars'].append(
+			{'name': k, 'content': v}
+		)
+	mandrill_client.messages.send_template(template_name, [], message)
