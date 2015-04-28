4d80be86b21c67f7bf181183600de8599e79f5e3
Minh Vu <vnm241191@gmail.com>
2015-04-27 18:36:01 -0700

    Try push to heroku

---
+++ config.py
@@ -0,0 +1,13 @@
+import os
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+CSRF_ENABLED = True
+SECRET_KEY = 'you-will-never-guess'
+
+SQLALCHEMY_DATABASE_URI = 'postgresql://testing_android:testing123@localhost/android'
+MANDRILL_API_KEY = os.getenv('MANDRILL_API_KEY')
+SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
+SQLALCHEMY_RECORD_QUERIES = True
+
+# slow database query threshold (in seconds)
+DATABASE_QUERY_TIMEOUT = 0.5
